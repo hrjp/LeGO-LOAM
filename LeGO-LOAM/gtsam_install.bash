7f24dc15ab4946fde2a0f4cebc1a223fb9a494d9
hrjp <r115159@g.kobe-kosen.ac.jp>
2023-03-09 13:46:05 +0000

    add build test

---
+++ LeGO-LOAM/gtsam_install.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install -y git curl wget zip unzip
+
+if [ ! -d "/home/$USER" ]; then
+    cd /home
+else
+    cd /home/$USER
+fi
+
+mkdir gtsam
+cd gtsam
+wget -O gtsam.zip https://github.com/borglab/gtsam/archive/4.1.1.zip
+unzip gtsam.zip -d .
+cd gtsam-4.1.1/
+mkdir build && cd build
+cmake ..
+sudo make install
